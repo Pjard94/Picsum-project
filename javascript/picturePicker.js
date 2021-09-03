1d9c76c22771140d3936d6e2f39e12726974b159
ChaosMiner65 <82098806+Pjard94@users.noreply.github.com>
2021-09-03 17:02:15 +0100

    Finished off the adding of elements and styled

--- javascript/picturePicker.js
+++ javascript/picturePicker.js
@@ -4,14 +4,19 @@ const applyButton = document.getElementById('picture-apply');
 const rerollButton = document.getElementById('picture-reroll');
 const picHolder = document.getElementById('picture-display');
 const url = 'https://picsum.photos/';
-let newPFP;
+var newPFP;
+let pictureID;
 let emailAddress;
 let previousEntry = false;
+let arrayPosition;
+let insidePosition;
+
+
 
 // Arrays
+let profileStart = [];
+let profiles = [];
 let profile = [];
-let profilePictures = [
-]
 
 window.addEventListener('load', function() {
     let picsumValue = Math.floor((Math.random() * (270 - 250 + 1)) + 250);
@@ -26,20 +31,30 @@ rerollButton.addEventListener('click', function(){
 applyButton.addEventListener('click', function validateEmail(){
     if(emailREGEX.test(emailInput.value) == true){
         let emailAddress = emailInput.value;
-        console.log(emailAddress);
-        let newPFP = picHolder.src;
-        console.log(newPFP)
+        newPFP = picHolder.src;
         for(let i = 0; i < profile.length; i++){
-            if( profile.indexOf(emailAddress) !== -1) {
+            if( profile.indexOf(emailAddress) == i) {
                 previousEntry = true;
-                console.log(profile[i])
+                arrayPosition = i;
+                profiles[i].push(newPFP);
+                document.getElementById(emailAddress).appendChild(document.createElement("img"));
+                document.getElementById(emailAddress).lastChild.setAttribute('src', newPFP);
             }
         }
         if(previousEntry === false) {
             profile.push(emailAddress)
+            profileStart.push(emailAddress);
+            profileStart.push(newPFP);
+            profiles.push(profileStart);
+            profileStart = [];
+            document.getElementById('submissions').appendChild(document.createElement("div"));
+            document.getElementById('submissions').lastChild.setAttribute('id', emailAddress);
+            document.getElementById('submissions').lastChild.classList.add('userSubmissions');
+            document.getElementById(emailAddress).appendChild(document.createElement("h3"));
+            document.getElementById(emailAddress).lastChild.append(emailAddress)
+            document.getElementById(emailAddress).appendChild(document.createElement("img"));
+            document.getElementById(emailAddress).lastChild.setAttribute('src', newPFP);
         }
-        console.log(profile)
-
 
         let picsumValue = Math.floor((Math.random() * (270 - 250 + 1)) + 250);
         picHolder.src=url + picsumValue;
