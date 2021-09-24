59eaaeb224a395428a0dc5cbbcc9f3c9f0988033
ChaosMiner65 <82098806+Pjard94@users.noreply.github.com>
2021-09-24 10:36:26 +0100

    Changed error action, and added action.

--- javascript/picturePicker.js
+++ javascript/picturePicker.js
@@ -4,35 +4,25 @@ const emailInput = document.getElementById('picum-email');
 const applyButton = document.getElementById('picture-apply');
 const rerollButton = document.getElementById('picture-reroll');
 const picHolder = document.getElementById('picture-display');
+const errorBar = document.getElementById('error-bar');
 // The url for shorthand
 const url = 'https://picsum.photos/';
 // empty variables to be used later in the code
 var newPFP;
-let pictureID;
-let emailAddress;
 let previousEntry = false;
 let arrayPosition;
-let insidePosition;
 
 // Arrays
 let profileStart = [];
 let profiles = [];
 let profile = [];
 
-// Loads an image as the page loads
-window.addEventListener('load', function() {
+function picturePicker() {
     let picsumValue = Math.floor((Math.random() * (270 - 250 + 1)) + 250);
     picHolder.src=url + picsumValue;
-}) 
+}
 
-// Loads a new image when the button is pressed
-rerollButton.addEventListener('click', function(){
-    let picsumValue = Math.floor((Math.random() * (270 - 250 + 1)) + 250);
-    picHolder.src=url + picsumValue;
-})
-
-// This will registar the submission and create areas for the images as well as submitting the new images later.
-applyButton.addEventListener('click', function validateEmail(){
+function validateFunction() {
     // This confirms that the email is valid.
     if(emailREGEX.test(emailInput.value) == true){
         let emailAddress = emailInput.value;
@@ -60,7 +50,7 @@ applyButton.addEventListener('click', function validateEmail(){
             document.getElementById('submissions').lastChild.setAttribute('id', emailAddress);
             document.getElementById('submissions').lastChild.classList.add('userSubmissions');
             document.getElementById(emailAddress).appendChild(document.createElement("h3"));
-            document.getElementById(emailAddress).lastChild.appendChild(titleNode)
+            document.getElementById(emailAddress).lastChild.appendChild(titleNode);
             document.getElementById(emailAddress).appendChild(document.createElement("img"));
             document.getElementById(emailAddress).lastChild.setAttribute('src', newPFP);
         }
@@ -68,9 +58,31 @@ applyButton.addEventListener('click', function validateEmail(){
         // This part randomises the image once submission is sucessful.
         let picsumValue = Math.floor((Math.random() * (270 - 250 + 1)) + 250);
         picHolder.src=url + picsumValue;
-    } else { // This part will activate if the email doesn't pass the REGEX
-        alert("Please fill input with a valid Email Address"); 
+
+    } else { // This part will activate if the email doesn't pass the REGEX 
+        errorBar.classList.add('validation-failed');
+        setTimeout(function () {
+            errorBar.classList.remove("validation-failed")
+          }, 3000)
     }
     // This resets the previous entry value otherwise it'll always read as true.
     previousEntry = false;
-});
+}
+
+// Loads an image as the page loads
+window.addEventListener('load', picturePicker);
+
+// This will registar the submission and create areas for the images as well as submitting the new images later.
+applyButton.addEventListener('click', validateFunction);
+
+document.addEventListener("keypress", function(e) {
+    var key = e.which || e.keyCode || 0;
+
+    if (key === 13) {
+        e.preventDefault;
+        applyButton.click(validateFunction);
+    }
+})
+
+// Loads a new image when the button is pressed
+rerollButton.addEventListener('click', picturePicker);
